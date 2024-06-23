47bc8ff299f3a6dc325eddeff67014756435b08d
Raquel Marie <raqueldelamer@gmail.com>
2024-06-23 15:22:18 -0500

    update js

--- tip-calculator.js
+++ tip-calculator.js
@@ -1,17 +1,23 @@
-function calculateTip() {
-    let price;
-    let tipPercent;
-    let tip;
-    price = prompt("What was your total bill?");
-    tipPercent = prompt("Percent would you like to tip (enter in decimals. For example 15% would be 0.15");
-    
-    // NOTE: The prompt returns values of the type String. We need to change the Strings into Numbers before we use them for math.
-    let priceNumber = Number(price);
-    let tipPercentNumber = Number(tipPercent);
-    tip = priceNumber * tipPercentNumber;
-    
-    priceParagraph.innerHTML = `Your total bill is ${priceNumber}`;
-    tipParagraph.innerHTML = `Your tip should be ${tip} at ${tipPercentNumber*100}%.`;
-  }
+function calculateTotal() {
+  let price;
+  let tipPercent;
+
+
+  price = prompt("What was your total bill?");
+  tipPercent = prompt("Percent would you like to tip (enter in decimals. For example 15% would be 0.15");
+ 
   
-  calculateButton.onclick = calculateTip;
+  // NOTE: The prompt returns values of the type String. We need to change the Strings into Numbers before we use them for math.
+  let priceNumber = Number(price);
+  let tipPercentNumber = Number(tipPercent);
+  let tipNumber = (priceNumber * tipPercentNumber);
+  let totalNumber = (priceNumber + tipNumber);
+
+ 
+  
+  priceParagraph.innerHTML = `Your total bill is ${priceNumber}`;
+  tipParagraph.innerHTML = `Your tip should be ${tipNumber}`;
+  totalParagraph.innerHTML = `Your total should be $ ${totalNumber}`;
+}
+
+calculateButton.onclick = calculateTotal;
